33c27e1d0896cc072ed0b0f47180d7be9b99632a
Robe Kyle P. Salangad <robekyle3@gmail.com>
2025-07-17 10:03:20 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+export default{
+  data(){
+    return {
+      username: '',
+      showusername: false
+    };
+  },
+  methods:{
+    toggleUsername() {
+      this.showusername = !this.showusername;
+    }
+    
+  }
+}
+
+</script>
+
+<template>
+ <div>
+  <input v-model="username" type="text" placeholder="Enter your username"/>
+  <button @click="toggleUsername">Toggle Username</button>
+  <p v-if="showusername">Your username is: {{ username }}</p>
+  <p v-else>Your username is hidden</p>
+  
+
+ </div>
+
+</template>
